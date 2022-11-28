2e0a04d79eae332214706d6e47ed3da9ba2b67bd
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2022-11-27 19:17:15 -0500

    feat: add source file and solve exercises

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+fn last(list: &[i32]) -> Option<&i32> {
+    match &list {
+        [] => None,
+        [x] => Some(x),
+        [_, rest @ ..] => last(rest),
+    }
+}
+
+fn last_two(list: &[i32]) -> Option<(&i32, &i32)> {
+    match &list {
+        [] => None,
+        [_] => None,
+        [x, y] => Some((x, y)),
+        [_, rest @ ..] => last_two(rest),
+    }
+}
+
 fn main() {
-    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
+    println!("{:?}", last(&[]));
+    println!("{:?}", last(&[1]));
+    println!("{:?}", last(&[1, 2]));
+    println!("{:?}", last(&[1, 2, 3]));
+    println!("{:?}", last_two(&[]));
+    println!("{:?}", last_two(&[1]));
+    println!("{:?}", last_two(&[1, 2]));
+    println!("{:?}", last_two(&[1, 2, 3]));
 }
